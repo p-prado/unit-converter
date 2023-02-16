83d03db3429ecd8640321e041dba24f4388f4b3e
Pablo Prado <pradopablo502@gmail.com>
2023-02-16 14:11:26 -0600

    Finish Minimum Viable Product

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
+const ejs = require("ejs");
 const app = express();
 const port = process.env.PORT;
 const calculator = require(`${__dirname}/calculator.js`);
 
+// Use express functionality to parse urlencoded responses 
 app.use(express.urlencoded({ extended: true }));
-
+// Set view engine to ejs
+app.set("view engine", "ejs");
+// Specify the location of the static files: public
+app.use(express.static("public"));
 
 app.route("/")
     .get(function (req, res) {
-        res.sendFile(`${__dirname}/index.html`);
+        res.render("home");
     });
-
+app.route("/convert")
+.get(function(req, res){
+    res.redirect("/");
+});
 app.post("/convert", function (req, res) {
     const fromNumber = req.body.fromNumber;
     const fromUnit = req.body.fromUnit;
     const toUnit = req.body.toUnit;
-
     const result = calculator.convert(fromNumber, fromUnit, toUnit);
-    res.send(`${result.fromNumber} ${result.fromUnit} = ${result.toNumber} ${result.toUnit}`);
+    res.render("results", {
+        fromNumber: result.fromNumber,
+        fromUnit: result.fromUnit,
+        toNumber: result.toNumber,
+        toUnit: result.toUnit
+    });
 });
-
 
 app.listen(port, function () {
     console.log(`Server listening on port ${port}`);
